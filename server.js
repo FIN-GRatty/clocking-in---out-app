4d5db2e0d1ae67f0bd1009658fc428d05bd45dc4
Todd Pencarinha <tpencarinha@altiras.onmicrosoft.com>
2025-08-20 16:53:17 -0500

    Remove static file serving for backend deployment

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'client/build')));
+// app.use(express.static(path.join(__dirname, 'client/build')));
 
 // Database setup
 const db = new sqlite3.Database('./clocking.db');
@@ -331,9 +331,9 @@ app.post('/api/admin/reset', (req, res) => {
 });
 
 // Serve React app for all other routes
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-});
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+// });
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
